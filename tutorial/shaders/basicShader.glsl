aad34b8da89dbb403ecd9881c45aa607f72352ba
nadavhaik <nadavhb@gmail.com>
2022-07-24 21:34:29 +0300

    comateti

--- tutorial/shaders/basicShader.glsl
+++ tutorial/shaders/basicShader.glsl
@@ -13,40 +13,41 @@ uniform vec4 lightDirection;
 out vec4 Color;
 void main()
 {
-	int kernelSize = 5;
-	int kernelSizeDiv = kernelSize/2;
-	int numOfNeigh = 0;
-	vec4 neighColor;
-	float posScales0[5] = float[5]( 0.003765,	0.015019,	0.023792,	0.015019,	0.003765);
-	float posScales1[5] = float[5]( 0.015019,	0.059912,	0.094907,	0.059912,	0.015019);
-	float posScales2[5] = float[5]( 0.023792,	0.094907,	0.0150342,	0.094907,	0.023792);
-	float posScales3[5] = float[5]( 0.015019,	0.059912,	0.094907,	0.059912,	0.015019);
-	float posScales4[5] = float[5]( 0.003765,	0.015019,	0.023792,	0.015019,	0.003765);
-	
-	for (int yCo = -kernelSizeDiv; yCo < kernelSizeDiv; yCo ++){
-		float posScale[5];
-		if (yCo + kernelSizeDiv == 0)
-			posScale = posScales0;
-		if (yCo + kernelSizeDiv == 1)
-			posScale = posScales1;
-		if (yCo + kernelSizeDiv == 2)
-			posScale = posScales2;
-		if (yCo + kernelSizeDiv == 3)
-			posScale = posScales3;
-		if (yCo + kernelSizeDiv == 4)
-			posScale = posScales4;
-
-		for (int xCo = -kernelSizeDiv; xCo < kernelSizeDiv; xCo ++){
-			float x = texCoord0.x * xCo;
-			float y = texCoord0.y * yCo;
-			numOfNeigh++;
-			neighColor += texture2D(sampler1, vec2(x,y)) * vec4(color0,0) * posScale[xCo + kernelSizeDiv];		
-		}
-	}
-	Color = neighColor;
-
+//	int kernelSize = 5;
+//	int kernelSizeDiv = kernelSize/2;
+//	int numOfNeigh = 0;
+//	vec4 neighColor;
+//	float posScales0[5] = float[5]( 0.003765,	0.015019,	0.023792,	0.015019,	0.003765);
+//	float posScales1[5] = float[5]( 0.015019,	0.059912,	0.094907,	0.059912,	0.015019);
+//	float posScales2[5] = float[5]( 0.023792,	0.094907,	0.0150342,	0.094907,	0.023792);
+//	float posScales3[5] = float[5]( 0.015019,	0.059912,	0.094907,	0.059912,	0.015019);
+//	float posScales4[5] = float[5]( 0.003765,	0.015019,	0.023792,	0.015019,	0.003765);
+//
+//	for (int yCo = -kernelSizeDiv; yCo < kernelSizeDiv; yCo ++){
+//		float posScale[5];
+//		if (yCo + kernelSizeDiv == 0)
+//			posScale = posScales0;
+//		if (yCo + kernelSizeDiv == 1)
+//			posScale = posScales1;
+//		if (yCo + kernelSizeDiv == 2)
+//			posScale = posScales2;
+//		if (yCo + kernelSizeDiv == 3)
+//			posScale = posScales3;
+//		if (yCo + kernelSizeDiv == 4)
+//			posScale = posScales4;
+//
+//		for (int xCo = -kernelSizeDiv; xCo < kernelSizeDiv; xCo ++){
+//			float x = texCoord0.x * xCo;
+//			float y = texCoord0.y * yCo;
+//			numOfNeigh++;
+//			neighColor += texture2D(sampler1, vec2(x,y)) * vec4(color0,0) * posScale[xCo + kernelSizeDiv];
+//		}
+//	}
+//	Color = neighColor;
+//
 
 	// reset the calculations
-	// Color = texture2D(sampler1, vec2(position0.x,position0.y))* vec4(color0,1.0) - neighColor/numOfNeigh; //you must have gl_FragColor
-	Color = texture2D(sampler1, texCoord0)* vec4(color0,1.0) - neighColor/numOfNeigh; //you must have gl_FragColor
+//	Color = texture2D(sampler1, vec2(position0.x,position0.y))* vec4(color0,1.0) - neighColor/numOfNeigh; //you must have gl_FragColor
+//	Color = texture2D(sampler1, texCoord0)* vec4(color0,1.0) - neighColor/numOfNeigh; //you must have gl_FragColor
+	Color = texture2D(sampler1, texCoord0)* vec4(color0,1.0) ; //you must have gl_FragColor
 }
